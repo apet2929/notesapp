fe7a67715a9cadff63f7dbe61bf46b0c40c7fb22
apet2929 <56840582+apet2929@users.noreply.github.com>
2021-08-11 09:46:58 -0500

    Added support for adding notes and dates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'notesapp';
-  notes = Array(5).fill(null);
+  notes = Array();
+  hasNotes : boolean;
+
+  ngOnInit() {
+
+    let numNotes = +localStorage.getItem("numNotes");
+    if(numNotes){
+      this.hasNotes = true;
+      for(let i = 0; i < numNotes; i++){
+        this.notes.push(null)
+      }
+    } else {
+      this.hasNotes = false;
+      this.notes.push(null);
+    }
+  }
+
+  reset(){
+    localStorage.clear();
+  }
 }
--- src/app/note/note.component.ts
+++ src/app/note/note.component.ts
@@ -8,7 +8,8 @@ import { FormBuilder } from '@angular/forms';
 })
 export class NoteComponent implements OnInit {
   @Input() index: number;
-  note: string
+  note: string;
+  dateCreated: Date;
   inputForm = this.formBuilder.group({
     noteInput : ""
   });
@@ -17,14 +18,56 @@ export class NoteComponent implements OnInit {
   ){};
 
   ngOnInit(){
-    this.note = localStorage.getItem("note" + this.index);
-    console.log(this.note);
+    // let note = localStorage.getItem("note" + this.index);
+    this.fromString(localStorage.getItem("note" + this.index));
   }
 
   onSubmit(){
+    if(!localStorage.getItem("note" + this.index)){
+      let numNotesStr = localStorage.getItem("numNotes");
+      if(numNotesStr){
+        let numNotes = +numNotesStr;
+        localStorage.setItem("numNotes", numNotes+1 + "");
+      } else {
+        localStorage.setItem("numNotes", "1");
+      }
+    }
     this.note = this.inputForm.get("noteInput").value;
-    localStorage.setItem("note" + this.index, this.note)
-    console.warn('Your order has been submitted', this.inputForm.value);
+    this.dateCreated = new Date(Date.now());
+    localStorage.setItem("note" + this.index, this.toString())
+    
+    console.log(this)
+    // console.warn('Your order has been submitted', this.inputForm.value);
     this.inputForm.reset();
+  }
+
+  toString(): string {
+    let str = ""
+    str += this.index + " (@) " + this.note + " (@) " + this.dateCreated;
+    console.log(str)
+    return str;
+  }
+
+  fromString(n: string){
+
+    console.log(n);
+    let endNumIndex = n.indexOf("(@)") - 1;
+    console.log("endNumIndex: " + endNumIndex)
+    let index = n.substring(0, endNumIndex);
+    console.log("Index: " + index);
+    let endNoteIndex = n.indexOf("(@)", endNumIndex + 5);
+    console.log("endNoteIndex: " + endNoteIndex)
+    let note = n.substring(endNumIndex + 5, endNoteIndex);
+    console.log("Note: " + note)
+    let date = n.substring(endNoteIndex + 4);
+    console.log("Date: " + date);
+
+    this.index = +index;
+    this.note = note;
+    this.dateCreated = new Date(date);
+    
+    console.log(this);
+    console.log("")
+    
   }
 }
